6b161f7e7c659e1e0edb5bc4451817c7440663e4
René Klatt <rene.klatt@spryker.com>
2021-12-03 08:20:07 +0100

    Removed unused configuration.

--- config/Shared/config_default-development.php
+++ config/Shared/config_default-development.php
@@ -2,13 +2,10 @@
 
 use Spryker\Shared\Application\ApplicationConstants;
 use Spryker\Shared\ErrorHandler\ErrorHandlerConstants;
-use Spryker\Shared\Kernel\KernelConstants;
 
 // ----------------------------------------------------------------------------
 // ------------------------------ CODEBASE ------------------------------------
 // ----------------------------------------------------------------------------
-
-$config[KernelConstants::STORE_PREFIX] = 'DEV';
 
 // >>> Debug
 $config[ApplicationConstants::ENABLE_APPLICATION_DEBUG]= true;
