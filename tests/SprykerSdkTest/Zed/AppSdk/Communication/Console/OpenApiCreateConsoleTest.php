9f7e556313b73d857b222e74ed06fc82c16cd3c7
Himanshu Verma <himanshu.verma@metacube.com>
2022-03-23 16:20:36 +0530

    add test case for open api create console

---
+++ tests/SprykerSdkTest/Zed/AppSdk/Communication/Console/OpenApiCreateConsoleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdkTest\Zed\AppSdk\Communication\Console;
+
+use Codeception\Test\Unit;
+use SprykerSdk\Zed\AppSdk\Communication\Console\AbstractConsole;
+use SprykerSdk\Zed\AppSdk\Communication\Console\OpenApiCreateConsole;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @group SprykerSdkTest
+ * @group Zed
+ * @group AppSdk
+ * @group Communication
+ * @group Console
+ * @group OpenApiCreateConsoleTest
+ */
+class OpenApiCreateConsoleTest extends Unit
+{
+    /**
+     * @var \SprykerSdkTest\Zed\AppSdk\CommunicationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testOpenApiCreateConsole(): void
+    {
+        $commandTester = $this->tester->getConsoleTester(new OpenApiCreateConsole());
+
+        // Act
+        $commandTester->execute([OpenApiCreateConsole::ARGUMENT_TITLE => 'Test File'], ['verbosity' => OutputInterface::VERBOSITY_VERBOSE]);
+
+        // Assert
+        $this->assertSame(AbstractConsole::CODE_SUCCESS, $commandTester->getStatusCode());
+    }
+}
