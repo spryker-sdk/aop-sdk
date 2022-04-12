4efae4c4720302c4337c631ba0169fcc4cb27c04
naveen-kumawat-metacube <naveen.kumawat@metacube.com>
2022-04-12 19:44:01 +0530

    OpenApi parser using cebe/php-openapi parser code refactoring and command list created

--- src/SprykerSdk/Zed/AopSdk/Business/OpenApi/Builder/OpenApiCodeBuilder.php
+++ src/SprykerSdk/Zed/AopSdk/Business/OpenApi/Builder/OpenApiCodeBuilder.php
@@ -9,6 +9,8 @@ namespace SprykerSdk\Zed\AopSdk\Business\OpenApi\Builder;
 
 use cebe\openapi\Reader;
 use cebe\openapi\spec\OpenApi;
+use cebe\openapi\spec\Operation;
+use cebe\openapi\spec\PathItem;
 use cebe\openapi\spec\Schema;
 use Generated\Shared\Transfer\MessageTransfer;
 use Generated\Shared\Transfer\OpenApiRequestTransfer;
@@ -20,6 +22,21 @@ use function Symfony\Component\String\u;
 class OpenApiCodeBuilder implements OpenApiCodeBuilderInterface
 {
     /**
+     * @var string
+     */
+    protected const APPSRESOURCE = 'AppsResource';
+
+    /**
+     * @var string
+     */
+    protected const SPRYKER = 'Spryker';
+
+    /**
+     * @var string
+     */
+    protected const CORE = 'core';
+
+    /**
      * @var \SprykerSdk\Zed\AopSdk\AopSdkConfig
      */
     protected AopSdkConfig $config;
@@ -28,6 +45,21 @@ class OpenApiCodeBuilder implements OpenApiCodeBuilderInterface
      * @var string
      */
     protected string $sprykMode = 'project';
+
+    /**
+     * @var string
+     */
+    protected string $moduleName = '';
+
+    /**
+     * @var string
+     */
+    protected string $organization = '';
+
+    /**
+     * @var array
+     */
+    protected array $parsedData = [];
 
     /**
      * @param \SprykerSdk\Zed\AopSdk\AopSdkConfig $config
@@ -46,14 +78,15 @@ class OpenApiCodeBuilder implements OpenApiCodeBuilderInterface
     {
         $openApiResponseTransfer = new OpenApiResponseTransfer();
         $openApi = $this->load($openApiRequestTransfer->getTargetFileOrFail());
-        
-        $organization = $openApiRequestTransfer->getOrganizationOrFail();
 
-        if ($organization === 'Spryker') {
-            $this->sprykMode = 'core';
+        //Set default value for organization
+        $this->organization = $openApiRequestTransfer->getOrganizationOrFail();
+
+        if ($this->organization === static::SPRYKER) {
+            $this->sprykMode = static::CORE; //Set sprykMode based on organization
         }
 
-        $openApiResponseTransfer = $this->buildCodeForOpenApi($openApi, $openApiResponseTransfer, $organization);
+        $openApiResponseTransfer = $this->buildCodeForOpenApi($openApi, $openApiResponseTransfer);
 
         if ($openApiResponseTransfer->getMessages()->count() === 0) {
             $messageTransfer = new MessageTransfer();
@@ -75,148 +108,220 @@ class OpenApiCodeBuilder implements OpenApiCodeBuilderInterface
     }
 
     /**
+     * @param array|string $resourceUri
+     *
+     * @return string
+     */
+    protected function getModuleName(string $resourceUri): string
+    {
+        $resources = explode('/', trim($resourceUri, '/'));
+
+        return ucwords(current($resources)) . 'Api';
+    }
+
+    /**
+     * @param string $resourceUri
+     *
+     * @return string
+     */
+    protected function getController(string $resourceUri): string
+    {
+        $controllerName = static::APPSRESOURCE;
+
+        $resources = explode('/', trim($resourceUri, '/'));
+
+        $lastResource = end($resources);
+
+        if (count($resources) > 1 && is_string($lastResource) && strpos($lastResource, '{') === false) {
+            $controllerName = $this->convertStringToTitle($lastResource);
+        }
+
+        return "{$controllerName}Controller";
+    }
+
+     /**
+      * @param string $resource
+      *
+      * @return string
+      */
+    protected function convertStringToTitle(string $resource): string
+    {
+        return u($resource)->camel()->title()->toString();
+    }
+
+    /**
      * @param \cebe\openapi\spec\OpenApi $openApi
      * @param \Generated\Shared\Transfer\OpenApiResponseTransfer|string $openApiResponseTransfer
-     * @param string $projectNamespace
      *
      * @return \Generated\Shared\Transfer\OpenApiResponseTransfer
      */
     protected function buildCodeForOpenApi(
         OpenApi $openApi,
-        OpenApiResponseTransfer $openApiResponseTransfer,
-        string $projectNamespace
+        OpenApiResponseTransfer $openApiResponseTransfer
     ): OpenApiResponseTransfer {
-        $parseData = [];
-
         /** @var \cebe\openapi\spec\PathItem $pathItem */
         foreach ($openApi->paths->getPaths() as $path => $pathItem) {
-            $resources = explode("/", trim($path,"/"));
-            $moduleName = ucwords(current($resources));
-            $controllerName = "AppsResource";
-
-            $lastResource = end($resources);
-
-            if(count($resources) > 1 && is_string($lastResource) && strpos($lastResource, "{") === false){
-                $controllerName = u($lastResource)->camel()->title()->toString();
-            }
-
-            /** @var \cebe\openapi\spec\Operation $operation */
-            foreach ($pathItem->getOperations() as $method => $operation) {
-
-                $parseData[$path][$method]["moduleName"] = "{$moduleName}Api";
-                $parseData[$path][$method]["controllerName"] = "{$controllerName}Controller";
-
-                /** @var \cebe\openapi\spec\Parameter|\cebe\openapi\spec\Reference $parameter */
-                foreach ($operation->parameters as $parameterKey => $parameter) {
-                    $parseData[$path][$method]['parameters'][$parameterKey] = json_decode(json_encode($parameter->getSerializableData()), true);
-                    $referencePath = $parameter->getDocumentPosition()->getPath();
-                    if (in_array('components', $referencePath)) {
-                        $parseData[$path][$method]['parameters'][$parameterKey]['reference'] = end($referencePath);
-                    }
-                }
-
-                if ($operation->requestBody) {
-                    /** @var \cebe\openapi\spec\RequestBody $mediaType */
-                    foreach ($operation->requestBody->content as $contentType => $mediaType) {
-                        $referencePath = $mediaType->schema->getDocumentPosition()->getPath();
-                        $parseData[$path][$method]['requestBody'] = $this->parseParameters($mediaType->schema, end($referencePath));
-                    }
-                }
-
-                /** @var \cebe\openapi\spec\Response|\cebe\openapi\spec\Reference $content */
-                foreach ($operation->responses as $status => $content) {
-                    if ($content->content) {
-                        foreach ($content->content as $contentType => $response) {
-                            $referencePath = $response->schema->getDocumentPosition()->getPath();
-                            $parseData[$path][$method]['responses'][$status] = $this->parseParameters($response->schema, end($referencePath));
-                        }
-                    } else {
-                        $parseData[$path][$method]['responses'][$status] = $content->description;
-                    }
-                }
-            }
+            //Parse data api path wise
+            $this->parseApiPathItem($path, $pathItem);
         }
 
-        $this->generateCommands($parseData, $projectNamespace);
-
-        dd($parseData);
+        //Generating Commands using parsed data
+        $this->generateCommands();
 
         return $openApiResponseTransfer;
     }
 
     /**
-     * @param \cebe\openapi\spec\Schema $schema
-     * @param string $className
-     *
-     * @return array
-     */
-    protected function parseParameters(Schema $schema, string $className): array
-    {
-        $response = [];
-
-        foreach ($schema->properties as $key => $schemaObject) {
-            if ($schemaObject->properties) {
-                return $this->parseParameters($schemaObject, $className);
-            } else {
-                $response[$className][] = "{$key}:{$schemaObject->type}";
-            }
-        }
-
-        return $response;
-    }
-
-    /**
-     * @param array $parseData
-     * @param string $projectNamespace
+     * @param string $path
+     * @param \cebe\openapi\spec\PathItem $pathItem
      *
      * @return void
      */
-    protected function generateCommands(array $parseData, string $projectNamespace): void
+    protected function parseApiPathItem(string $path, PathItem $pathItem): void
+    {
+        /** @var \cebe\openapi\spec\Operation $operation */
+        foreach ($pathItem->getOperations() as $method => $operation) {
+            //Set Module Name
+            $this->parsedData[$path][$method]['moduleName'] = $this->getModuleName($path);
+            //Set Controller Name
+            $this->parsedData[$path][$method]['controllerName'] = $this->getController($path);
+
+            if ($operation->requestBody) {
+                //Set Request Body by parsing
+                $this->parseRequestBody($operation, $path, $method);
+            }
+
+            //Set Response Body by parsing
+            $this->parseResponseBody($operation, $path, $method);
+        }
+    }
+
+    /**
+     * @param \cebe\openapi\spec\Operation $operation
+     * @param string $path
+     * @param string $method
+     *
+     * @return void
+     */
+    protected function parseRequestBody(Operation $operation, string $path, string $method): void
+    {
+        /** @var \cebe\openapi\spec\RequestBody $mediaType */
+        foreach ($operation->requestBody->content as $mediaType) {
+            $this->requestBodyParser($mediaType->schema, $this->getPathUsingSchema($mediaType->schema), $path, $method);
+        }
+    }
+
+    /**
+     * @param \cebe\openapi\spec\Schema $schema
+     * @param string $className
+     * @param string $path
+     * @param string $method
+     *
+     * @return void
+     */
+    protected function requestBodyParser(Schema $schema, string $className, string $path, string $method): void
+    {
+        foreach ($schema->properties as $key => $schemaObject) {
+            if ($schemaObject->properties) {
+                $this->requestBodyParser($schemaObject, $className, $path, $method);
+            }
+            $this->parsedData[$path][$method]['requestBody'][$className][] = "{$key}:{$schemaObject->type}";
+        }
+    }
+
+    /**
+     * @param \cebe\openapi\spec\Operation $operation
+     * @param string $path
+     * @param string $method
+     *
+     * @return void
+     */
+    protected function parseResponseBody(Operation $operation, string $path, string $method): void
+    {
+        /** @var \cebe\openapi\spec\Response|\cebe\openapi\spec\Reference $content */
+        foreach ($operation->responses as $status => $content) {
+            if ($content->content) {
+                foreach ($content->content as $response) {
+                    $this->responseParser($response->schema, $this->getPathUsingSchema($response->schema), $path, $method);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param \cebe\openapi\spec\Schema $schema
+     * @param string $className
+     * @param string $path
+     * @param string $method
+     *
+     * @return void
+     */
+    protected function responseParser(Schema $schema, string $className, string $path, string $method): void
+    {
+        foreach ($schema->properties as $key => $schemaObject) {
+            if ($schemaObject->properties) {
+                $this->responseParser($schemaObject, $className, $path, $method);
+            }
+            $this->parsedData[$path][$method]['responses'][$className][] = "{$key}:{$schemaObject->type}";
+        }
+    }
+
+    /**
+     * @param \SprykerSdk\Zed\AopSdk\Business\OpenApi\Builder\cebe\openapi\spec\Schema $schema
+     *
+     * @return string
+     */
+    protected function getPathUsingSchema(Schema $schema): string
+    {
+        $referencePath = $schema->getDocumentPosition()->getPath();
+
+        return end($referencePath);
+    }
+
+    /**
+     * @return void
+     */
+    protected function generateCommands(): void
     {
         $commandLines = [];
 
-        foreach ($parseData as $uri => $operations) {
+        foreach ($this->parsedData as $uri => $operations) {
             foreach ($operations as $method => $data) {
-                if(isset($data["requestBody"])){
-                    foreach ($data["requestBody"]  as $command => $parameters) {
+                if (isset($data['requestBody'])) {
+                    foreach ($data['requestBody'] as $command => $parameters) {
                         $commandLines[$command] = [
                             'vendor/bin/spryk-run',
                             'AddSharedTransferProperty',
                             '--mode', $this->sprykMode,
-                            '--organization', $projectNamespace,
+                            '--organization', $this->organization,
                             '--module', $data['moduleName'],
-                            '--name', "{$command}Transfer",
+                            '--name', $command,
                             '--propertyName', implode(',', $parameters),
                             '-n',
                             '-v',
                         ];
                     }
                 }
-                
-                if(isset($data["responses"])){
-                    foreach ($data["responses"] as $response) {
-                        if(is_array($response)){
-                            foreach ($response as $command => $parameters) {
-                                $commandLines[$command] = [
-                                    'vendor/bin/spryk-run',
-                                    'AddSharedTransferProperty',
-                                    '--mode', $this->sprykMode,
-                                    '--organization', $projectNamespace,
-                                    '--module', $data['moduleName'],
-                                    '--name', "{$command}Transfer",
-                                    '--propertyName', implode(',', $parameters),
-                                    '-n',
-                                    '-v',
-                                ];
-                            }
-                        }
+
+                if (isset($data['responses'])) {
+                    foreach ($data['responses'] as $command => $parameters) {
+                        $commandLines[$command] = [
+                            'vendor/bin/spryk-run',
+                            'AddSharedTransferProperty',
+                            '--mode', $this->sprykMode,
+                            '--organization', $this->organization,
+                            '--module', $data['moduleName'],
+                            '--name', $command,
+                            '--propertyName', implode(',', $parameters),
+                            '-n',
+                            '-v',
+                        ];
                     }
                 }
             }
         }
 
         dd(array_values($commandLines));
-
     }
 
     /**
