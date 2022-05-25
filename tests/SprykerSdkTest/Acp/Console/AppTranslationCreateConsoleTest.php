1fbfa3a785568198bcb02a0d5a2fd13dec835e19
stereomon <rene.klatt@spryker.com>
2022-05-25 09:45:33 +0200

    APPS-4270 Added test.

--- tests/SprykerSdkTest/Acp/Console/AppTranslationCreateConsoleTest.php
+++ tests/SprykerSdkTest/Acp/Console/AppTranslationCreateConsoleTest.php
@@ -141,7 +141,38 @@ class AppTranslationCreateConsoleTest extends Unit
     /**
      * @return void
      */
-    public function testAsksTranslationAndHandleEmptyValueThenShouldLeftTheProcess(): void
+    public function testAsksForMissingTranslationsAndAbortAfterFirstTranslationWasEntered(): void
+    {
+        // Arrange
+        $this->tester->haveManifestsAndConfigurationFile();
+        $commandTester = $this->tester->getConsoleTester(AppTranslationCreateConsole::class);
+
+        // Act
+        $commandTester->setInputs([
+            'en_US', // Locales from manifest found, console asks to select a locale I'd like to add translations for.
+            'Foo Bar', // First found missing translation.
+            '', // Empty value to leave the current process.
+            'No', // Would you like to add new translations?
+            'No', // Would you like to add translations for another locale?
+        ]);
+        $commandTester->execute([]);
+
+        // Assert
+        $this->assertSame(AbstractConsole::CODE_SUCCESS, $commandTester->getStatusCode());
+        $this->assertStringContainsString('We found the following locales, please select one you\'d like to define translations for:', $commandTester->getDisplay());
+        $this->assertStringContainsString('We found missing translations for the locale en_US', $commandTester->getDisplay());
+        $this->assertStringContainsString('The following inputs will be used for your selected locale en_US', $commandTester->getDisplay());
+        $this->assertStringContainsString('When you like to leave the process hit the Enter key until you see "Left the process, will continue with next steps". Already entered data will be automatically saved.', $commandTester->getDisplay());
+        $this->assertStringContainsString('Please enter a translation for foo.bar: ', $commandTester->getDisplay());
+        $this->assertStringContainsString('Left the process, will continue with next steps.', $commandTester->getDisplay());
+        $this->assertStringContainsString('Would you like to add new translations?', $commandTester->getDisplay());
+        $this->assertStringContainsString('Would you like to add translations for another locale?', $commandTester->getDisplay());
+    }
+
+    /**
+     * @return void
+     */
+    public function testAsksForMissingTranslationsAndAbortWhenNoTranslationWasEntered(): void
     {
         // Arrange
         $this->tester->haveValidManifestFiles(['en_US', 'de_DE']);
