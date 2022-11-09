d7a297d80937351b2abdf1ae6aa5fb8301afc71a
Bohdan Turchyk <bohdan.turchyk@spryker.com>
2022-11-09 12:41:37 +0200

    APPS-4846 CR fix

--- src/SprykerSdk/Acp/Configuration/Reader/AppConfigurationReader.php
+++ src/SprykerSdk/Acp/Configuration/Reader/AppConfigurationReader.php
@@ -26,6 +26,8 @@ class AppConfigurationReader implements AppConfigurationReaderInterface
     /**
      * @param string $filePath
      *
+     * @throws \SprykerSdk\Acp\Exception\AppConfigurationException
+     * 
      * @return string
      */
     protected function readFile(string $filePath): string
