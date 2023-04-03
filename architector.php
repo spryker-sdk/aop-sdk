e5b4e5b626cdc32af75eed321524503e49154707
Daniel Santos <daniel.santos@spryker.com>
2023-04-03 09:24:07 +0100

    PBC-996: Skip RemoveUselessVarTagRector rule...Add early return in AppConfigurationCreateConsole::getPropertyName

--- architector.php
+++ architector.php
@@ -11,11 +11,11 @@ use Rector\Core\Configuration\Option;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
 use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeOrIfReturnToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\Return_\ReturnBinaryAndToEarlyReturnRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 
 defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', __DIR__);
@@ -36,9 +36,6 @@ return static function (RectorConfig $rectorConfig) {
         RemoveUselessReturnTagRector::class,
         ReturnBinaryAndToEarlyReturnRector::class,
         SimplifyUselessVariableRector::class,
-    ]);
-
-    $rectorConfig->ruleWithConfiguration(TypedPropertyRector::class, [
-        TypedPropertyRector::INLINE_PUBLIC => true,
+        RemoveUselessVarTagRector::class,
     ]);
 };
